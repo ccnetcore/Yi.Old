06fa117004e37aa838fead4fc50bbe492ab21b3b
橙子 <454313500@qq.com>
2021-10-18 19:02:26 +0800

    添加修改密码dto

---
+++ Yi.Framework/Yi.Framework.DTOModel/ChangePwdDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yi.Framework.Model.Models;
+
+namespace Yi.Framework.DTOModel
+{
+    public class ChangePwdDto:user
+    {
+        public string oldPassword { get; set; }
+    }
+}
