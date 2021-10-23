49afd9395f39e82a649f8bf0b15dfde0d24d2c9f
橙子 <454313500@qq.com>
2021-10-23 17:49:36 +0800

    初始化数据库

--- Yi.Framework/Yi.Framework.Model/DbInit/DataSeed.cs
+++ Yi.Framework/Yi.Framework.Model/DbInit/DataSeed.cs
@@ -21,10 +21,54 @@ namespace Yi.Framework.Model.DbInit
                     roles = new List<role>()
                     {
                         new role()
-                        { 
+                        {
+                            role_name="管理员",
                             menus = new List<menu>()
                             {
-                                new menu() { mould=new mould()}
+                                new menu() 
+                                { 
+                                    menu_name="用户角色管理",is_show=1,children=new List<menu>()
+                                    {
+                                        new menu()
+                                        { 
+                                            menu_name="用户管理",router="/AdmUser", is_show=1,children=new List<menu>()
+                                            {
+                                                new menu()
+                                                {
+                                                    menu_name="get",is_show=0,
+                                                    mould=new mould()
+                                                    {
+                                                        mould_name="get",url="/user/getuser"
+                                                    }
+                                                },
+                                                new menu()
+                                                {
+                                                    menu_name="update",is_show=0,
+                                                    mould=new mould()
+                                                    {
+                                                        mould_name="update",url="/user/updateuser"
+                                                    }
+                                                },
+                                                new menu()
+                                                {
+                                                    menu_name="del",is_show=0,
+                                                    mould=new mould()
+                                                    {
+                                                        mould_name="del",url="/user/dellistUser"
+                                                    }
+                                                },
+                                                new menu()
+                                                {
+                                                    menu_name="add",is_show=0,
+                                                    mould=new mould()
+                                                    {
+                                                        mould_name="del",url="/user/adduser"
+                                                    }
+                                                }
+                                            }    
+                                        }
+                                    }   
+                                }
                             }
                         }
                     }
