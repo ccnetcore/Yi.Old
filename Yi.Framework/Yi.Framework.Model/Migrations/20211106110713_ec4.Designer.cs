6e7ab2325adaf5bcbb66cd43cb4334cd8141d1d5
橙子 <454313500@qq.com>
2021-11-06 19:11:43 +0800

    提交迁移文件

--- Yi.Framework/Yi.Framework.Model/Migrations/20211106110713_ec4.Designer.cs
+++ Yi.Framework/Yi.Framework.Model/Migrations/20211106110713_ec4.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Yi.Framework.Model;
 
 namespace Yi.Framework.Model.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20211106110713_ec4")]
+    partial class ec4
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
