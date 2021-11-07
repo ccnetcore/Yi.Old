f6ab793450a37db7867a861bbce14ca4ec21fdc4
橙子 <454313500@qq.com>
2021-11-07 12:46:41 +0800

    添加Goods

---
+++ Yi.Framework/Yi.Framework.Model/Search/PageResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yi.Framework.Model.Search
+{
+	public class  PageResult<T>
+	{
+        public static readonly long serialVersionUID = 4612105649493688532L;
+        public long total; // 总记录数
+        public int totalPages; //总页数
+        public List<T> rows; // 每页显示的数据集合
+
+        public PageResult(long total, List<T> rows)
+        {
+            this.total = total;
+            this.rows = rows;
+        }
+    }
+}
---
+++ Yi.Framework/Yi.Framework.Service/SearchService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yi.Framework.Interface;
+using Yi.Framework.Model.Models;
+using Yi.Framework.Model.Search;
+
+namespace Yi.Framework.Service
+{
+    public class SearchService : ISearchService
+    {
+        private IGoodsService _goodsService;
+        public SearchService(IGoodsService goodsService)
+        {
+            _goodsService = goodsService;
+        }
+        public void ImpDataBySpu()
+        {
+            ImportToEs();
+        }
+        private void ImportToEs()
+        {
+            int page = 1;
+            int size;
+            int rows = 100;
+            do
+            {
+                List<Goods> goodsList = new List<Goods>();
+                // 上架商品
+                PageResult<spu> result = _goodsService.QuerySpuByPage(page, rows, null, true);
+                List<spu> spus = result.rows;
+                size = spus.Count;
+                foreach (var spu in spus)
+                {
+                    try
+                    {
+                        Goods g = BuildGoods(spu);
+                        // 处理好的数据添加到集合中
+                        goodsList.Add(g);
+
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        continue;//部分数据不严格
+                    }
+                }
+                // 存入es,先留着，不写
+                //_elasticSearchService.Send(goodsList);
+                page++;
+            } while (size == 100);
+        }
+
+        private Goods BuildGoods(spu spu)
+        {
+            Goods goods = new Goods();
+            return goods;
+        }
+    }
+}
