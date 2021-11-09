12dbc64b74f412a8dcf5a6f75affa22f2f25094e
橙子 <454313500@qq.com>
2021-11-09 17:01:26 +0800

    searchmodel

---
+++ Yi.Framework/Yi.Framework.Model/Search/SearchRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.Framework.Model.Search
+{ 
+    public class SearchRequest
+    {
+        public static readonly int DEFAULT_PAGE = 1;
+        public static readonly int DEFAULT_SIZE = 20;
+        public string key { get; set; }
+        public int page { get; set; }
+        //排序字段
+        public string sortBy { get; set; }
+        //是否降序
+        public bool descending { get; set; }
+        //过滤字段
+        public Dictionary<string, string> filter = new Dictionary<string, string>();
+
+        public int getPage()
+        {
+            if (page == 0)
+            {
+                return DEFAULT_PAGE;
+            }
+            // 获取页码时做一些校验，不能小于1
+            return Math.Max(DEFAULT_PAGE, page);
+        }
+
+        public int getSize()
+        {
+            return DEFAULT_SIZE;
+        }
+
+
+    }
+}
---
+++ Yi.Framework/Yi.Framework.Model/Search/SearchResult.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yi.Framework.Model.Models;
+
+namespace Yi.Framework.Model.Search
+{
+    public class SearchResult<Goods> : PageResult<Goods>
+    {
+        public List<brand> brands = new List<brand>();
+        public List<category> categories = new List<category>();
+        //规格参数过滤条件
+        public List<Dictionary<string, object>> specs = new List<Dictionary<string, object>>();
+        public SearchResult(long total,
+                       int totalPage,
+                       List<Goods> items,
+                       List<category> categories,
+                       List<brand> brands,
+                       List<Dictionary<string, object>> specs) : base
+            (total, items)
+        {
+
+            this.categories = categories;
+            this.brands = brands;
+            this.specs = specs;
+        }
+
+    }
+}
