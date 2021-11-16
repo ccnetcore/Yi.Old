daec6c79b7ef1646f594558f4d49275422afbf1a
橙子 <454313500@qq.com>
2021-11-16 13:55:16 +0800

    订单微服务

---
+++ Yi.Framework/Yi.Framework.DTOModel/CartDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.Framework.DTOModel
+{
+	public class CartDto
+	{
+		public long skuId { get; set; }//商品skuId
+		public int num { get; set; } //购买数量
+	}
+}
---
+++ Yi.Framework/Yi.Framework.DTOModel/OrderDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yi.Framework.DTOModel
+{
+    public class OrderDto
+    {
+        public long addressId { get; set; } // 收获人地址id
+        public byte paymentType { get; set; }// 付款类型
+        public List<CartDto> carts { get; set; }// 订单详情
+    }
+}
